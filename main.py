e40449f046cd05d43ef2a31ff7cc1c8d8cb5f971
github_astrokota <bikesdakota@gmail.com>
2024-12-20 23:52:05 +0000

    added character count and report

--- main.py
+++ main.py
@@ -4,20 +4,46 @@ def main():
         file_contents = f.read()
 
     word_count = count_words(file_contents)
- 
-    return word_count
-    
 
+    print("--- Begin report of books/frankenstein.txt ---")
+  
+    print(f"{word_count} words found in the document")
+
+    print("")
+    
+    characters_dict = character_count(file_contents)
+
+    for character in characters_dict:
+        print(f"The '{character}' character was found {characters_dict[character]} times")
+
+    print("--- End report ---")
+    
 def count_words(file_contents):
 
+    return len(get_words(file_contents))
+
+def character_count(file_contents):
+
+    characters = {}
+    words = get_words(file_contents)
+
+    for word in words:
+        for character in word:
+            if character.isalpha():
+                lower = character.lower()
+                if lower in characters:
+                    characters[lower] += 1
+                else:
+                    characters[lower] = 1
+
+    return characters
+   
+def get_words(file_contents):
+
     if file_contents.strip() == "":
-        return 0
+        return []
 
-    words = file_contents.split()
-
-    return len(words)
-
-    
+    return file_contents.split()
 
 main()
 
